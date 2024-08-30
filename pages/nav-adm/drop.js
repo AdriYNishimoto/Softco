ec91c496df4b850c3dc45db690a2a72b6c85b8dc
Adriano Yudi R. Nishimoto <rnishimotoyudi123@gmail.com>
2024-08-30 12:05:33 -0400

    nav finish

---
+++ pages/nav-adm/drop.js
@@ -0,0 +1,6 @@
+var btnPerfil = document.querySelector('#btn-perfil');
+var dropdown = document.querySelector('#dropdown');
+
+btnPerfil.addEventListener('click', function(){
+    dropdown.classList.toggle('show');
+});
