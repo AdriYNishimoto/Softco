e64bd3a6b25ec4875dee4a65d512a8da4011ddd4
Adriano Yudi R. Nishimoto <rnishimotoyudi123@gmail.com>
2024-08-30 19:52:11 -0400

    modified

--- pages/adm/perfil/drop.js
+++
@@ -1,6 +0,0 @@
-var btnPerfil = document.querySelector('#btn-perfil');
-var dropdown = document.querySelector('#dropdown');
-
-btnPerfil.addEventListener('click', function(){
-    dropdown.classList.toggle('show');
-});
