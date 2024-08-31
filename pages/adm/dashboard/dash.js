83598f5b97d344873d810e4972d1891df2e8a622
Adriano Yudi R. Nishimoto <rnishimotoyudi123@gmail.com>
2024-08-30 21:29:24 -0400

    add graphic in dash.html

---
+++ pages/adm/dashboard/dash.js
@@ -0,0 +1,18 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar', // Você pode alterar para 'line', 'pie', etc.
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Dados simples',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
